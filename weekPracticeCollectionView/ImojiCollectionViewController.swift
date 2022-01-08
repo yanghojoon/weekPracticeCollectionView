d71752bceeb24f93306fccd4ff3b72c9ae348dfc
yanghojoon <yanghojoon8487@gmail.com>
2022-01-08 21:02:25 +0900

    feat: 셀이 가로방향으로 스크롤되도록 수정

--- weekPracticeCollectionView/ImojiCollectionViewController.swift
+++ weekPracticeCollectionView/ImojiCollectionViewController.swift
@@ -11,13 +11,18 @@ import UIKit
 
 class ImojiCollectionViewController: UICollectionViewController {
     private let reuseIdentifier = "Cell"
-    let emojies = ["🔥", "🥰", "🥲", "👍", "👨‍🔬", "🤪", "🐸", "⚾️"]
-    let flowLayout = UICollectionViewFlowLayout() // 클래스이고 UICollectionViewLayout을 상속받고 있다. 
+    let emojies = ["🔥", "🥰", "🥲", "👍", "👨‍🔬", "🤪", "🐸", "⚾️", "🥎", "🎱", "🪀", "🤾🏻", "🎬"]
+    let flowLayout = UICollectionViewFlowLayout() // 클래스이고 UICollectionViewDelegate을 상속받고 있다.
+    // 시각적으로 보이는 것도 어떻게 보면 유저와의 상호작용이라고 볼 수 있다.
     
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.delegate = self
+        flowLayout.minimumLineSpacing = 20
+        flowLayout.scrollDirection = .horizontal
         collectionView.collectionViewLayout = flowLayout
+        collectionView.isPagingEnabled = false // 트루로 하면 20의 간격만큼 스크롤이 밀리는 문제 발생
+        collectionView.decelerationRate = .fast
     }
 
     // MARK: UICollectionViewDataSource
@@ -53,10 +58,19 @@ class ImojiCollectionViewController: UICollectionViewController {
 }
 
 // MARK: UICollectionViewDelegateFlowLayout
+// Therefore, implement this protocol on the object assigned to your collection view’s delegate property.
 extension ImojiCollectionViewController: UICollectionViewDelegateFlowLayout { // 동적인 상황에 대응하기 위함, 그게 아니라면 그냥 있는 flowlayout을 사용하는 것이 낫다.
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = collectionView.frame.width / 4 - 10
+        let width = collectionView.frame.width
 
         return CGSize(width: width, height: width * 1.3)
+    }
+}
+
+// MARK: ScrollViewDelegate
+extension ImojiCollectionViewController { // UICollectionViewDelegate가 UIScrollViewDelegate를 상속하고 있다. UICollectionViewDelegateFlowLayout이 UICollectionViewDelegate를 상속하고 있다.
+    
+    // 아래 메서드는 사용자가 스크롤을 하고 스크린과 손이 떨어졌을 때 호출되는 메서드이다.
+    override func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
     }
 }
