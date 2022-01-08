57ff85f688766076f5b0ed32365742cc709ec1d8
yanghojoon <yanghojoon8487@gmail.com>
2022-01-08 20:15:19 +0900

    feat: 한줄에 4개의 셀이 나올 수 있도록 구현

--- weekPracticeCollectionView/ImojiCollectionViewCell.swift
+++ weekPracticeCollectionView/ImojiCollectionViewCell.swift
@@ -8,5 +8,20 @@
 import UIKit
 
 class ImojiCollectionViewCell: UICollectionViewCell {
+    let imojiLabel = UILabel()
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.addSubview(imojiLabel)
+        setupLabelLayout()
+    }
+    
+    private func setupLabelLayout() {
+        imojiLabel.translatesAutoresizingMaskIntoConstraints = false
+        imojiLabel.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        imojiLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        imojiLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        imojiLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        imojiLabel.textAlignment = .center
+    }
 }
--- weekPracticeCollectionView/ImojiCollectionViewController.swift
+++ weekPracticeCollectionView/ImojiCollectionViewController.swift
@@ -12,10 +12,12 @@ import UIKit
 class ImojiCollectionViewController: UICollectionViewController {
     private let reuseIdentifier = "Cell"
     let emojies = ["🔥", "🥰", "🥲", "👍", "👨‍🔬", "🤪", "🐸", "⚾️"]
+    let flowLayout = UICollectionViewFlowLayout() // 클래스이고 UICollectionViewLayout을 상속받고 있다. 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        collectionView.delegate = self
+        collectionView.collectionViewLayout = flowLayout
     }
 
     // MARK: UICollectionViewDataSource
@@ -25,17 +27,36 @@ class ImojiCollectionViewController: UICollectionViewController {
     }
 
 
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    override func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
         return emojies.count
     }
-
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
     
-    
+    override func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: reuseIdentifier,
+            for: indexPath) as? ImojiCollectionViewCell else {
+                return ImojiCollectionViewCell()
+            }
+        cell.imojiLabel.text = emojies[indexPath.item]
+        cell.backgroundColor = .yellow
         return cell
     }
 
     // MARK: UICollectionViewDelegate
 
+}
+
+// MARK: UICollectionViewDelegateFlowLayout
+extension ImojiCollectionViewController: UICollectionViewDelegateFlowLayout { // 동적인 상황에 대응하기 위함, 그게 아니라면 그냥 있는 flowlayout을 사용하는 것이 낫다.
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width / 4 - 10
+
+        return CGSize(width: width, height: width * 1.3)
+    }
 }
