be42d7c66126beea382907c0416337e7c20acbae
yanghojoon <yanghojoon8487@gmail.com>
2022-01-22 12:30:34 +0900

    refactor: lineSpacing을 줘도 페이지가 밀리지 않도록 수정
    Line Spacing의 절반씩 inset을 줘서 페이지가 밀리지 않도록 수정

--- weekPracticeCollectionView/ImojiCollectionViewController.swift
+++ weekPracticeCollectionView/ImojiCollectionViewController.swift
@@ -14,6 +14,7 @@ class ImojiCollectionViewController: UICollectionViewController {
     let emojies = ["🔥", "🥰", "🥲", "👍", "👨‍🔬", "🤪", "🐸", "⚾️", "🥎", "🎱", "🪀", "🤾🏻", "🎬"]
     let flowLayout = UICollectionViewFlowLayout() // 클래스이고 UICollectionViewDelegate을 상속받고 있다.
     // 시각적으로 보이는 것도 어떻게 보면 유저와의 상호작용이라고 볼 수 있다.
+    let minimumLineSpace = CGFloat(20)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +22,7 @@ class ImojiCollectionViewController: UICollectionViewController {
         flowLayout.minimumLineSpacing = 20
         flowLayout.scrollDirection = .horizontal
         collectionView.collectionViewLayout = flowLayout
-        collectionView.isPagingEnabled = false // 트루로 하면 20의 간격만큼 스크롤이 밀리는 문제 발생
+        collectionView.isPagingEnabled = true // 트루로 하면 20의 간격만큼 스크롤이 밀리는 문제 발생
         collectionView.decelerationRate = .fast
 //        flowLayout.itemSize = CGSize(width: <#T##CGFloat#>, height: <#T##CGFloat#>) // 디폴트값은 50, 50
     }
@@ -61,23 +62,29 @@ class ImojiCollectionViewController: UICollectionViewController {
 // MARK: UICollectionViewDelegateFlowLayout
 // Therefore, implement this protocol on the object assigned to your collection view’s delegate property.
 extension ImojiCollectionViewController: UICollectionViewDelegateFlowLayout { // 동적인 상황에 대응하기 위함, 그게 아니라면 그냥 있는 flowlayout을 사용하는 것이 낫다.
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = collectionView.frame.width
+//        let width = collectionView.frame.width
+        let newWidth = UIScreen.main.bounds.width - minimumLineSpace
 
-        return CGSize(width: width, height: width * 1.3)
+        return CGSize(width: newWidth, height: collectionView.frame.size.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: minimumLineSpace / 2, bottom: 0, right: minimumLineSpace)
     }
 }
 
 // MARK: ScrollViewDelegate
-extension ImojiCollectionViewController { // UICollectionViewDelegate가 UIScrollViewDelegate를 상속하고 있다. UICollectionViewDelegateFlowLayout이 UICollectionViewDelegate를 상속하고 있다.
-    
-    // 아래 메서드는 사용자가 스크롤을 하고 스크린과 손이 떨어졌을 때 호출되는 메서드이다.
-    override func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
-        
-        // velocity는 가속도. 천천히 움직일 수도 있고 빠르게 움직일 수도 있다. (빠를 때에만 페이지를 이동하도록)
-        print(#function)
-        let cellIncludingSpace = collectionView.bounds.width + flowLayout.minimumLineSpacing
-        print(cellIncludingSpace)
-        targetContentOffset.pointee = CGPoint(x: cellIncludingSpace, y: 0)
-    }
-}
+//extension ImojiCollectionViewController { // UICollectionViewDelegate가 UIScrollViewDelegate를 상속하고 있다. UICollectionViewDelegateFlowLayout이 UICollectionViewDelegate를 상속하고 있다.
+//
+//    // 아래 메서드는 사용자가 스크롤을 하고 스크린과 손이 떨어졌을 때 호출되는 메서드이다.
+//    override func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+//
+//        // velocity는 가속도. 천천히 움직일 수도 있고 빠르게 움직일 수도 있다. (빠를 때에만 페이지를 이동하도록)
+//        print(#function)
+//        let cellIncludingSpace = collectionView.bounds.width + flowLayout.minimumLineSpacing
+//        print(cellIncludingSpace)
+//        targetContentOffset.pointee = CGPoint(x: cellIncludingSpace, y: 0)
+//    }
+//}
